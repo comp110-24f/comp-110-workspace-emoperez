5d2bdb337086f770ed75e49b883a3e075c8f36f9
Lisbeth Morales <emoperez@unc.edu>
2024-08-23 12:35:37 -0400

    Finished Exercise 00!

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,8 @@
+"""My first exercise in COMP 110!"""
+
+__author__ = "730801797"
+
+
+def greet(name: str) -> str:
+    """A welcoming first function definition."""
+    return "Hello, " + name + "!"
