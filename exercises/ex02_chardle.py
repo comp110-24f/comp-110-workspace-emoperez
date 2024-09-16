62da1aedd212f2df6e179754f5e88e9ec7a2a225
Lisbeth Morales <emoperez@unc.edu>
2024-09-16 16:11:08 -0400

    Progress on Exercise 1

--- exercises/ex02_chardle.py
+++ exercises/ex02_chardle.py
@@ -18,7 +18,7 @@ def input_letter() -> str:
     """Will exit run if the length of user_letter isn't 1."""
     user_letter: str = input("Enter a single character: ")
     if len(user_letter) != 1:
-        print("Error: Charcter must be a single character.")
+        print("Error: Character must be a single character.")
         exit()
     return user_letter
 
@@ -28,24 +28,31 @@ def contains_char(word: str, letter: str) -> None:
     """Searches through each index of the word to see where it matches with the letter."""
     """If it does match then it adds to the count of instances and prints out a message at where each instance was found."""
     print("Searching for " + letter + " in " + word)
-    instances: int = int(0)
+    instances: str = str("No instances")
+    count: int = int(0)
     if len(word) == 5 and len(letter) == 1:
         if word[0] == letter[0]:
-            instances = int(instances + 1)
+            count = count + 1
             print(letter + " found at index 0")
         if word[1] == letter[0]:
-            instances = int(instances + 1)
+            count = count + 1
             print(letter + " found at index 1")
         if word[2] == letter[0]:
-            instances = int(instances + 1)
+            count = count + 1
             print(letter + " found at index 2")
         if word[3] == letter[0]:
-            instances = int(instances + 1)
+            count = count + 1
             print(letter + " found at index 3")
         if word[4] == letter[0]:
-            instances = int(instances + 1)
+            count = count + 1
             print(letter + " found at index 4")
-    print(str(instances) + " instances of " + letter + " found in " + word)
+        if count > 1:
+            instances = str(str(count) + " instances")
+        elif count == 1:
+            instances = str(str(count) + " instance")
+        else:
+            instances = str("No instances")
+    print(instances + " of " + letter + " found in " + word)
 
 
 def main() -> None:
